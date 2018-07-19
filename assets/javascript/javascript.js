25a584707a93dfc8b8e22f70ee400566b0f76918
Hayley K <mrs.kornilenko@gmail.com>
2018-07-19 12:11:09 -0400

    updated

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -30,7 +30,7 @@ $('document').ready(function() {
 
   var apiURL =
     'https://api.giphy.com/v1/gifs/search?api_key=76fkjhw48rHiwuZ361P9ku9owicnknH7&q=';
-  var gifLimit = 1;
+  var gifLimit = 10;
   var gifOffsetRating = '&offset=0&rating=G&lang=en';
 
   function clickGIFLoad() {
@@ -46,93 +46,19 @@ $('document').ready(function() {
       var results = response.data;
       $('.gifs-load-here').empty();
 
-      results.forEach(element => {
-        console.log(element);
-        var ratingGif = element.rating.toUpperCase();
-        console.log(ratingGif);
-
-        var imgRef = $(element).attr(
-          'src',
-          element.images.fixed_height_still.url
-        );
-
-        var dataStill = $(element).attr(
-          'data-still',
-          element.images.fixed_height_still.url
-        );
-        // 'data-still',
-        // this.images.fixed_height_still.url,
-        // 'data-animate',
-        // this.images.fixed_height.url,
-        // 'data-state',
-        // 'still'
-        // );
-        console.log(imgRef);
-        console.log(dataStill);
-
-        // gif.on('click', animateGif);
-
-        // var card =
-        //   '<div class="row"><div class="col-sm-6"><div class="card"> <img class="card-img-top"' +
-        //   imgRef +
-        //   '<div class="card-body"><p class="card-text"> Rating: ' +
-        //   ratingGif +
-        //   '</p></div></div></div>';
-
-        // $('.gifs-load-here').html(card);
+      results.forEach(function(result) {
+        output =
+          "<div class='card' style='width: 18rem;'>" +
+          "<img class='card-img-top' src='" +
+          result.images.fixed_height.url +
+          "' alt='Card image cap'>" +
+          "<div class='card-body'>" +
+          "<p class='card-text'> This Giphy rating is " +
+          result.rating +
+          '</p> </div> </div>';
       });
 
-      // for (var i = 0; i < results.length; i++) {
-      //   const gifIndex = results[i];
-
-      //   var ratingGif = gifIndex.rating.toUpperCase();
-
-      //   var imgRef = gifIndex.attr(
-      //     'src',
-      //     gifIndex.images.fixed_height_still.url,
-      //     'data-still',
-      //     gifIndex.images.fixed_height_still.url,
-      //     'data-animate',
-      //     gifIndex.images.fixed_height.url,
-      //     'data-state',
-      //     'still'
-      //   );
-
-      //   gifIndex.on('click', animateGif);
-
-      //   var card =
-      //     '<div class="row"><div class="col-sm-6"><div class="card"> <img class="card-img-top"' +
-      //     imgRef +
-      //     '<div class="card-body"><p class="card-text"> Rating: ' +
-      //     ratingGif +
-      //     '</p></div></div></div>';
-
-      // var showDiv = $('<div class="card" style="width: 18rem;">');
-
-      // var cardBody = $('<div class="card-body">');
-      // var p = $('<p class="card-text">').text(
-      //   'Rating: ' + results[i].rating.toUpperCase()
-      // );
-
-      // var showGIF = $('<img class="card-img-top" id="gif">');
-
-      // showGIF.attr('src', results[i].images.fixed_height_still.url);
-      // showGIF.attr('data-still', results[i].images.fixed_height_still.url);
-      // showGIF.attr('data-animate', results[i].images.fixed_height.url);
-      // showGIF.attr('data-state', 'still');
-
-      // showGIF.on('click', animateGif);
-
-      // // LOAD ON SCREEN
-      // gridRow.prepend(gridCol);
-      // gridCol.append(showDiv);
-      // showDiv.append(showGIF);
-      // showGIF.append(cardBody);
-      // cardBody.append(p);
-
-      //   $('.gifs-load-here').html(card);
-      //   console.log(card);
-      // }
+      $('.gifs-load-here').html(output);
     });
   }
 
